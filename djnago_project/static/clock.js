b8f4a4d0bbf9d6ca497e12bbca56ed64dcb27164
gutenLee <3bnm1128@naver.com>
2020-06-12 04:40:14 +0900

    June,04

--- djnago_project/static/clock.js
+++ djnago_project/static/clock.js
@@ -81,66 +81,154 @@
 	    target.style.backgroundColor = "#FFFAF0";
 	};
 
-     function bring_data(result){
+     function bring_data(realtimeStationArrival){
         alert("hello");
+
+        var result = realtimeStationArrival
+
         var up = [];
         var down = [];
         var outbound = [];
         var inbound = [];
         var count = 0;
-        console.log(result)
+
+        if( result == 0 ){
+             var target = document.getElementById("station");
+             target.innerHTML = "<strong class = 'subway_info' >" + "열차 없습니다" +"</strong>";
+        };
 
         for(i=0; i < result.length; i++){
 
-            if (result[i]['subwayId'] == line_code){
-                alert(result[i]['subwayId'])
-                if(result[i]['updnLine'] == '상행' || result[i]['updnLine'] == '내선'){
+            if(result[i]['updnLine'] == '상행' || result[i]['updnLine'] == '내선'){
 
-                    var target = document.getElementById("up");
-                    var new_span = document.createElement('span');
-                    new_span.innerHTML = "<strong class = 'subway_info' >" + result[i]['arvlMsg2'] +"</strong>";
-                    target.appendChild(new_span);
+                var target = document.getElementById("up");
+                var new_span = document.createElement('li');
+                new_span.innerHTML = "<strong class = 'subway_info' >" + result[i]['arvlMsg2'] +"</strong>";
+                target.appendChild(new_span);
 
-                    var target2 = document.getElementById("up2");
-                    var new_span2 = document.createElement('span');
-                    new_span2.innerHTML = "<strong class = 'subway_info' >" + result[i]['arvlMsg3'] +"</strong>";
-                    target2.appendChild(new_span2);
+                var target2 = document.getElementById("up2");
+                var new_span2 = document.createElement('li');
+                new_span2.innerHTML = "<strong class = 'subway_info' >" + result[i]['arvlMsg3'] +"</strong>";
+                target2.appendChild(new_span2);
 
-                };
-                if(result[i]['updnLine'] == '하행' || result[i]['updnLine'] == '외선'){
-                    var target = document.getElementById("down");
-                    var new_span = document.createElement('span');
-                    new_span.innerHTML = "<strong class = 'subway_info' >" + result[i]['arvlMsg2'] +"</strong>";
-                    target.appendChild(new_span);
+            };
+            if(result[i]['updnLine'] == '하행' || result[i]['updnLine'] == '외선'){
+                var target = document.getElementById("down");
+                var new_span = document.createElement('li');
+                new_span.innerHTML = "<strong class = 'subway_info' >" + result[i]['arvlMsg2'] +"</strong>";
+                target.appendChild(new_span);
 
-                    var target2 = document.getElementById("down2");
-                    var new_span2 = document.createElement('span');
-                    new_span2.innerHTML = "<strong class = 'subway_info' >" + result[i]['arvlMsg3'] +"</strong>";
-                    target2.appendChild(new_span2);
-
-                };
+                var target2 = document.getElementById("down2");
+                var new_span2 = document.createElement('li');
+                new_span2.innerHTML = "<strong class = 'subway_info' >" + result[i]['arvlMsg3'] +"</strong>";
+                target2.appendChild(new_span2);
             };
         };
      };
 
+  function fcstTrainFontColor() {
+    var target = document.getElementsByClassName("subway_info")
+    target.style.color="Red";
+  }
+
+// 설정된 시간의 += 15분 내의 지하철 시간표
   function timetable(up, down){
         var left_time = [];
+
         var now = new Date();
 		var s = now.getSeconds();
 		var m = now.getMinutes();
 		var h = now.getHours();
 
 		console.log(s, m, h)
-
+    //  상행선 시간표
     for(i=0; i < up.length; i ++){
+
         var target = document.getElementById("upp");
         var new_span = document.createElement('li');
-        new_span.innerHTML = "<strong class = 'subway_info' >" + up[i][0] +"</strong>";
+
+        var time = up[i][0].split(':')
+        var arriveTime = time[0] + " 시 " + time[1] + " 분"
+        var heading = " (" + up[i][2] + " 행)"
+
+        new_span.innerHTML = "<strong class = 'subway_info' >" + arriveTime + heading +"</strong>";
         target.appendChild(new_span);
 
-        var test = up[i][0].split(':')
-        console.log(test)
-        console.log(test[0]-h)
+        // H : M : S
+        var target_time = up[i][0].split(':')
+        console.log(target_time)
+        console.log(target_time[0]-h)
     };
+    //  하행선 시간표
+    for(i=0; i < down.length; i ++){
+        var target = document.getElementById("downn");
+        var new_span = document.createElement('li');
 
+        var time = down[i][0].split(':')
+        var arriveTime = time[0] + " 시 " + time[1] + " 분"
+        var heading = " (" + down[i][2] + " 행)"
+
+        new_span.innerHTML = "<strong class = 'subway_info' >" + arriveTime + heading +"</strong>";
+        target.appendChild(new_span);
+
+        //  H : M : S
+        var target_time = down[i][0].split(':')
+        console.log(target_time)
+        console.log(target_time[0]-h)
+    };
   };
+
+
+   function recommendClothing(pop, now_temp, findDust){
+
+            if(findDust[0] > 60 && findDust[1] > 70){
+                var target = document.getElementById("mask")
+                target.innerText = "KF90 마스크 챙기세요"
+            }
+            if(findDust[0] < 50 && findDust[1] < 50){
+                var target = document.getElementById("mask")
+                target.innerText = "필요없어요"
+            }
+
+            if (pop > 50){
+                var target = document.getElementById("umbrella")
+                target.innerText = '우산 챙겨요!'
+            }
+            else if(pop < 50){
+                alert("fuc")
+                var target = document.getElementById("umbrella")
+                target.innerText = '필요없음!'
+            };
+
+            if ( now_temp < 5){
+                  var target = document.getElementById("clothing")
+                  target.innerHTML = "현재 너무 추워요.<br> 패딩, 두꺼운코트,<br> 목도리, 기모가 좋겠네요"
+            }
+            else if ( 5 <= now_temp && now_temp < 9){
+                  var target = document.getElementById("clothing")
+                  target.innerHTML = "현재 추워요.<br>  코트,경량패딩, 히트텍,<br> 니트, 레깅스가 좋겠네요"
+
+            }
+            else if ( 9 <= now_temp && now_temp < 17){
+                  var target = document.getElementById("clothing")
+                  target.innerHTML = "현재 쌀쌀해요.<br> 자켓, 트렌치코트,<br>야상, 니트, 청바지가 좋겠네요"
+
+            }
+            else if ( 17 <= now_temp && now_temp < 20){
+                  var target = document.getElementById("clothing")
+                  target.innerHTML = "현재 서늘해요.<br> 맨투맨, 얇은 니트, 가디건,<br> 청바지가 좋겠네요"
+            }
+            else if ( 20<= now_temp && now_temp < 23){
+                  var target = document.getElementById("clothing")
+                  target.innerHTML = "현재 선선해요.<br> 긴팔티, 얇은 가디건,<br> 면바지, 청바지가 좋겠네요"
+            }
+            else if (23 <= now_temp && now_temp < 26){
+                  var target = document.getElementById("clothing")
+                  target.innerHTML =  "<td class = 'outfit' >" + "반팔, 얇은 셔츠,<br> 반바지, 면바지가<br> 좋겠네요" +"</td>";
+            }
+            else if ( 26 <= now_temp && now_temp < 40 ){
+                  var target = document.getElementById("clothing")
+                  target.innerHTML ="현재 기온 더워요<br> 민소매, 반바지,<br> 원피스가 좋겠네요"
+            };
+
+        };
