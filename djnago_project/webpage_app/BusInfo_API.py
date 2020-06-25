e9088149adba2dacc9f78e0b7afe65e7b95d0ba4
gutenLee <3bnm1128@naver.com>
2020-06-25 22:11:24 +0900

    좌표기반 버스정류장 조회

--- djnago_project/webpage_app/BusInfo_API.py
+++ djnago_project/webpage_app/BusInfo_API.py
@@ -1,55 +1,44 @@
 import json
-import urllib
-from urllib.parse import quote_plus
-from urllib.parse import urlencode
-from urllib.parse import unquote
-from urllib.request import Request, urlopen
-
-
-key = 'vzJIPq38c6Mw7Q5fmDYv4vFn1o5fMO2Ml4Z8u5fQMTZERE4iU8aXePUDLelmk8TYl7Iy3iRfebpeKqsvqMsxYQ%3D%3D'
-
-url = 'http://openapi.tago.go.kr/openapi/service/ArvlInfoInqireService/getCtyCodeList'
-
+from pprint import pprint
+from urllib.parse import urlencode, quote_plus
+from urllib.request import urlopen
 import requests
+import xmltodict
+from urllib3.util import url
 
-def get_request_query(url, operation, serviceKey):
-    import urllib.parse as urlparse
-    # params = urlparse.urlencode(params)
-    request_query = url + '/' + operation + '?' + '&' + 'serviceKey' + '=' + serviceKey
-    return request_query
-
-
-# 요청 URL과 오퍼레이션
-URL = 'http://openapi.tago.go.kr/openapi/service/ArvlInfoInqireService'
-OPERATION = 'getCtyCodeList' # 국경일 + 공휴일 정보 조회 오퍼레이션
-
-# 파라미터
-SERVICEKEY = 'vzJIPq38c6Mw7Q5fmDYv4vFn1o5fMO2Ml4Z8u5fQMTZERE4iU8aXePUDLelmk8TYl7Iy3iRfebpeKqsvqMsxYQ%3D%3D'
-# solYear  = '2018'  # 연도
-# solMonth = '09'   # 월
-# PARAMS = {'solYear':solYear, 'solMonth':solMonth}
+with open('APIkey/API_keys.json') as json_file:
+    json_data = json.load(json_file)
+json_busKey = json_data['busInfo_API']
+api_key_decode = requests.utils.unquote(json_busKey)
 
 
+# 지정된 좌표와 반경 범위 내의 정류소 목록을 조회한다.
+url = 'http://ws.bus.go.kr/api/rest/stationinfo/getStationByPos'
+queryParams = '?' + urlencode({quote_plus('ServiceKey'): api_key_decode,
+                               quote_plus('tmX'): '126.98633090000001',
+                               quote_plus('tmY'): '37.563398',
+                               quote_plus('radius'): '500'})
 
-import requests
-api_key = "vzJIPq38c6Mw7Q5fmDYv4vFn1o5fMO2Ml4Z8u5fQMTZERE4iU8aXePUDLelmk8TYl7Iy3iRfebpeKqsvqMsxYQ%3D%3D"
-api_key_decode = requests.utils.unquote(api_key)
-parameters = {"ServiceKey":api_key_decode}
-req = requests.get(url, params = parameters)
-request = Request(url + parameters)
-response_body = urlopen(request).read()
-print(req)
-print(response_body)
+request = urlopen(url + queryParams)
+print(url + queryParams)
+data = request.read().decode('utf8')
+json_data = json.loads(json.dumps(xmltodict.parse(data)))
+data = json_data['ServiceResult']['msgBody']['itemList']
+
+stationOnMap = []
+for i in range(0, len(data)):
+    a = {
+          "gpsX": str(data[i]['gpsX']),
+          "gpsY": str(data[i]['gpsY']),
+          "stationNm": str(data[i]['stationNm'])
+    }
+    stationOnMap.append(a)
 
 
-
-# request_query = get_request_query(URL, OPERATION, SERVICEKEY)
-# print('request_query:', request_query)
-# response = requests.get(url=request_query)
-# print('status_code:' + str(response.status_code))
-#
-#
-# if True == response.ok:
-#     print(response.text)
-#
-# get_request_query(URL, OPERATION, SERVICEKEY)
+    # a = {"arsId": data[i]['arsId'],
+    #       "dist": data[i]['dist'],
+    #       "gpsX": str(data[i]['gpsX']),
+    #       "gpsY": str(data[i]['gpsY']),
+    #       "stationId": str(data[i]['stationId']),
+    #       "stationNm": str(data[i]['stationNm']),
+    #       "stationTp": str(data[i]['stationTp'])}
